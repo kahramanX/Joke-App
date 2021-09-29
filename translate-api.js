88480c45b26ab89692407a9663acf81cc1297524
Mustafa Kahraman <80291704+kahramanX@users.noreply.github.com>
2021-09-29 19:56:09 +0300

    update to yandex translate api

--- translate-api.js
+++ translate-api.js
@@ -1,18 +1,13 @@
 class TranslateApi{
-    constructor(englishJoke){
-        this.baseUrl="https://nlp-translation.p.rapidapi.com"
+    constructor(){
+        this.baseUrl="https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20210929T092515Z.b198b0b39cf9f0ae.e49acfa0ca099ae922d25ff8c79b9fa08b456abe&text=hello,%20my%20name%20is%20atilla&lang=tr"
         this.translateApi = axios.create({
             baseURL : this.baseUrl,
-            params: {text: 'Hello, world!!', to: 'es', from: 'en'},
-            headers: {
-                'x-rapidapi-host': 'nlp-translation.p.rapidapi.com',
-                'x-rapidapi-key': '75aa3a9944msh08a6701b65d3e25p1e6426jsndaa26a1a387e'
-            }
         });
     }
 
    async getTranslateApi(){
-       let translateObject = await this.translateApi.get("/v1/translate");
+       let translateObject = await this.translateApi.post("");
         console.log(translateObject);
     }
 }
